0f561fdcda4fbafbf5323d521c0f691c96c9c5d3
kumaran2048 <kumarans2048.sse@saveetha.com>
2025-09-29 13:26:39 +0530

    updates

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api')
