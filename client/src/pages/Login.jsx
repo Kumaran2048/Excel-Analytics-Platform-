a75dd9fe48fbc425385f0003d6ecbd8544672c7e
kumaran2048 <kumarans2048.sse@saveetha.com>
2025-11-04 18:46:30 +0530

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -153,7 +153,14 @@ const Login = () => {
                       Remember me
                     </label>
                   </div>
-
+                  <div className="text-sm">
+                    <Link
+                      to="/forgot-password"
+                      className="font-medium text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300"
+                    >
+                    </Link>
+                  </div>
+                </div>
 
                 <div>
                   <button
