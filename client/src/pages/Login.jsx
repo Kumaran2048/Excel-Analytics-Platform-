6b9521ac5eb03bdb6387c47c357f5d3f5c398527
kumaran2048 <kumarans2048.sse@saveetha.com>
2025-11-04 18:45:05 +0530

    change

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -153,15 +153,7 @@ const Login = () => {
                       Remember me
                     </label>
                   </div>
-                  <div className="text-sm">
-                    <Link
-                      to="/forgot-password"
-                      className="font-medium text-green-600 hover:text-green-500 dark:text-green-400 dark:hover:text-green-300"
-                    >
-                      Forgot password?
-                    </Link>
-                  </div>
-                </div>
+
 
                 <div>
                   <button
